e7eac16d9b5d16de068c29262f0fe0cdb057d724
Joses Vito Chandra <josesvc@gmail.com>
2021-06-15 22:49:22 +0700

    Update home.vue to show alert when there are expiring items

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,6 +19,7 @@
 
 <script>
 // @ is an alias to /src
+import axios from "axios";
 import MainHeader from "@/components/navigations/MainHeader";
 import DashboardSidebar from "@/components/navigations/DashboardSidebar"
 // import HelloWorld from '@/components/HelloWorld.vue'
@@ -29,6 +30,33 @@ export default {
     MainHeader,
     DashboardSidebar,
     // HelloWorld
+  },
+  data() {
+    return {
+      expiring: []
+    }
+  },
+  mounted() {
+    if(this.$store.getters.getUserData.role_id == 3) {
+      const query = '?sdate=1970-01-01&edate=2099-12-31'
+        axios.get(process.env.VUE_APP_API_URL + 'product/procurement' + query, { headers: {
+          token: this.$store.getters.getToken
+        }})
+        .then(res => this.expiring = res.data.values.filter(el => {
+          const dateExpWarning = new Date(el.date_exp)
+          dateExpWarning.setDate(dateExpWarning.getDate() - 12)
+          if(el.date_procured && el.is_dismissed == 0 && new Date() >= dateExpWarning) return el
+        }))      
+    }
+  },
+  watch: {
+    expiring(newValue) {
+      this.$bvToast.toast('You have ' + newValue.length + ' expiring item(s)\nPlease check your inventory', {
+        noCloseButton: true,
+        solid: true,
+        variant: 'info',
+      })
+    }
   }
 }
 </script>
